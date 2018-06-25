0bde6425567f932db873c49700214eae4f564cf6
Stainie <stanko.pirot@yahoo.com>
2018-06-26 01:48:19 +0200

    controllers

---
+++ app/controllers/controller.js
@@ -0,0 +1,25 @@
+exports.test = (req, res, next) => {
+    console.log('Heroku');
+    res.send('Heroku');
+};
+
+exports.getWebHook = (req, res, next) => {
+
+    let VERIFY_TOKEN = "stankos_verification_token";
+
+    let mode = req.query['hub.mode'];
+    let token = req.query['hub.verify_token'];
+    let challenge = req.query['hub.challenge'];
+
+    if (mode && token) {
+
+        if (mode === 'subscribe' && token === VERIFY_TOKEN) {
+
+            console.log('WEBHOOK_VERIFIED');
+            res.status(200).send(challenge);
+
+        } else {
+            res.sendStatus(403);
+        }
+    }
+};
--- app/routes/routes.js
+++ app/routes/routes.js
@@ -1,30 +1,9 @@
 const express = require('express');
 const router = express.Router();
+const controller = require('../controllers/controller');
 
-router.get('/', (req, res, next) => {
-    console.log('Heroku');
-    res.send('Heroku');
-});
+router.get('/', controller.test);
 
-router.get('/webhook', (req, res, next) => {
-
-    let VERIFY_TOKEN = "stankos_verification_token";
-
-    let mode = req.query['hub.mode'];
-    let token = req.query['hub.verify_token'];
-    let challenge = req.query['hub.challenge'];
-
-    if (mode && token) {
-
-        if (mode === 'subscribe' && token === VERIFY_TOKEN) {
-
-            console.log('WEBHOOK_VERIFIED');
-            res.status(200).send(challenge);
-
-        } else {
-            res.sendStatus(403);
-        }
-    }
-});
+router.get('/webhook', controller.getWebHook);
 
 module.exports = router;
