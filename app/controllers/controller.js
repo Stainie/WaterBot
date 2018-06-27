8517e1118f5c0d0bcb5ff31b3c344adf47da728b
Stainie <stanko.pirot@yahoo.com>
2018-06-28 01:13:54 +0200

    Name Response 6

--- app/controllers/controller.js
+++ app/controllers/controller.js
@@ -138,8 +138,9 @@ exports.postWebhook = (req, res, next) => {
                     console.log('Error: ' + error);
                 }
             });
-            console.log("RESPONSE FROM PAYLOAD: " + bodyObj.first_name);
-            response = { "text": `Hi "${bodyObj.first_name}"! I will be your personal water trainer :) you can call me Nada Macura` };
+            console.log("RESPONSE FROM PAYLOAD: ");
+            response = { "text": "Thanks!" };
+            //response = { "text": `Hi "${bodyObj.first_name}"! I will be your personal water trainer :) you can call me Nada Macura` };
         }
         // Send the message to acknowledge the postback
         callSendAPI(sender_psid, response);
