c6b1972f42e07b14da788620035941e8b6074af2
Stainie <stanko.pirot@yahoo.com>
2018-06-30 22:43:00 +0200

    Keep alive

--- mainserver.js
+++ mainserver.js
@@ -4,5 +4,9 @@ const port = process.env.PORT || 1238;
 
 const server = http.createServer(app);
 
-
 server.listen(port, () => console.log(`Server started on port ${port}`));
+
+setInterval(() => {
+    http.get("http://blooming-river-72137.herokuapp.com");
+    console.log("keep alive");
+}, 300000);
